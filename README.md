ac1b6073204713453ef3ac09fa34d376f33eac3b
Jaime Abril Lázaro <jablazr@gmail.com>
2024-02-24 13:16:36 +0100

    Change modid (#1)
    * change mod id
    
    * Change modid and package name
    
    * Update README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Enhanced Equipment Mod
+
+- Java 17
+- Forge 49.0.30
+- Minecraft 1.20.4
+
+## Development guide
+
+Start client with `gradle runClient`.
--- src/main/java/com/jablazr/enhanced_equipment/EnhancedEquipmentMod.java
+++ src/main/java/com/jablazr/enhanced_equipment/EnhancedEquipmentMod.java
@@ -1,6 +1,9 @@
-package com.example.examplemod;
+package com.jablazr.enhanced_equipment;
+
+import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.food.FoodProperties;
@@ -27,14 +30,13 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(EnhancedEquipmentMod.MODID)
+public class EnhancedEquipmentMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "enhanced_equipment";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -61,7 +63,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public EnhancedEquipmentMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
