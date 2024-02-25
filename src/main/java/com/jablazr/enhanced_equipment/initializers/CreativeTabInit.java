2290d8943ec64349c865cdce51a8040ffe3a9a9f
jablazr <jablazr@gmail.com>
2024-02-25 16:29:20 +0100

    Add cobalt block Add spotless code formatter

---
+++ src/main/java/com/jablazr/enhanced_equipment/initializers/CreativeTabInit.java
@@ -0,0 +1,43 @@
+package com.jablazr.enhanced_equipment.initializers;
+
+import com.jablazr.enhanced_equipment.EnhancedEquipmentMod;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+@Mod.EventBusSubscriber(
+    modid = EnhancedEquipmentMod.MODID,
+    bus = Mod.EventBusSubscriber.Bus.MOD,
+    value = Dist.CLIENT)
+public class CreativeTabInit {
+  public static final DeferredRegister<CreativeModeTab> TABS =
+      DeferredRegister.create(Registries.CREATIVE_MODE_TAB, EnhancedEquipmentMod.MODID);
+
+  public static final List<Supplier<? extends ItemLike>> MOD_TAB_ITEMS = new ArrayList<>();
+
+  public static final RegistryObject<CreativeModeTab> MOD_TAB =
+      TABS.register(
+          "enhanced_equipment_tab",
+          () ->
+              CreativeModeTab.builder()
+                  .title(Component.translatable("creative_mode.tab"))
+                  .icon(ItemInit.COBALT_BLOCK_ITEM.get()::getDefaultInstance)
+                  .displayItems(
+                      (displayParams, output) ->
+                          MOD_TAB_ITEMS.forEach(itemLike -> output.accept(itemLike.get())))
+                  .build());
+
+  public static <T extends Item> RegistryObject<T> addToTab(RegistryObject<T> itemLike) {
+    MOD_TAB_ITEMS.add(itemLike);
+    return itemLike;
+  }
+}
